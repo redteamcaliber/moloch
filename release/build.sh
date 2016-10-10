face5cb84e3cf29edca32c42a872f55d49c56f83
Andy Wick <andywick@gmail.com>
2016-10-10 07:23:26 -0400

    updates

--- release/build.sh
+++ release/build.sh
@@ -2,17 +2,20 @@
 yara_version=1.7
 geoip_version=1.6.0
 pcap_version=1.7.4
-curl_version=7.50.2
+curl_version=7.50.3
 glib2_dir=2.48
 glib2_version=2.48.2
 node_version=0.10.46
 daq_version=2.0.6
+lua_version=5.3.3
 
 export PATH=/data/moloch/bin:$PATH
 cd /tmp/moloch
-./configure --with-libpcap=../libpcap-${pcap_version} --with-yara=../yara-${yara_version} --with-GeoIP=../GeoIP-${geoip_version} --with-curl=../curl-${curl_version} --with-glib2=../glib-${glib2_version}
+./configure --with-libpcap=../libpcap-${pcap_version} --with-yara=../yara-${yara_version} --with-GeoIP=../GeoIP-${geoip_version} --with-curl=../curl-${curl_version} --with-glib2=../glib-${glib2_version} --with-lua=../lua-${lua_version}
 make clean
 make
+(cd capture/plugins/lua ; make)
+(cd capture/plugins/pfring ; make)
 make install
 
 # Copy files
--- release/thirdparty.sh
+++ release/thirdparty.sh
@@ -2,11 +2,12 @@
 yara_version=1.7
 geoip_version=1.6.0
 pcap_version=1.7.4
-curl_version=7.50.2
+curl_version=7.50.3
 glib2_dir=2.48
 glib2_version=2.48.2
-node_version=0.10.46
+node_version=4.6.0
 daq_version=2.0.6
+lua_version=5.3.3
 
 cd /tmp
 builddir=`pwd`
@@ -20,6 +21,7 @@ wget -N http://curl.haxx.se/download/curl-${curl_version}.tar.gz
 wget -N http://ftp.gnome.org/pub/gnome/sources/glib/${glib2_dir}/glib-${glib2_version}.tar.xz
 wget -N https://nodejs.org/download/release/v${node_version}/node-v${node_version}-linux-x64.tar.xz
 wget -N https://www.snort.org/downloads/snort/daq-${daq_version}.tar.gz
+wget -N https://www.lua.org/ftp/lua-${lua_version}.tar.gz
 
 # dir setup
 mkdir /data /data/moloch /data/moloch/etc /data/moloch/bin /data/moloch/logs /data/moloch/raw 
@@ -53,5 +55,9 @@ tar zxf curl-${curl_version}.tar.gz
 # glib2
 xzcat glib-${glib2_version}.tar.xz | tar xf -
 (cd glib-${glib2_version} ; ./configure --disable-xattr --disable-shared --enable-static --disable-libelf --disable-selinux --with-pcre=internal; make)
+
+# lua
+tar zxf lua-${lua_version}.tar.gz
+(cd lua-${lua_version}; make MYCFLAGS=-fPIC linux)
 
 exit 0
