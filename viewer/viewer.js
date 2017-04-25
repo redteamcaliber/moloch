90ae9f72268766a34fe853945644d48f775a333d
31453 <elyse.rinne@teamaol.com>
2017-04-25 10:04:21 -0400

    use different localStorage for new UI decodings

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1671,27 +1671,7 @@ app.post('/user/columns/delete', checkCookieToken, function(req, res) {
 });
 
 app.get('/decodings', function(req, res) {
-  var settings = decode.settings();
-  var decodeItems = {};
-  for (var key in settings) {
-    var setting = settings[key];
-    var obj = {name: setting.name || key};
-    if (setting.title || setting.fields) {
-      obj.fields = {};
-      if (setting.title) {
-        obj.fields.title = {name: setting.title, disabled: true};
-      }
-      obj.fields[key + ":enabled"] = {name: "Enable", type: "checkbox", disabled:true};
-      setting.fields.forEach(function(field) {
-        obj.fields[key + ":" + field.key] = {name: field.name || field.key, type: field.type};
-      });
-      decodeItems[key] = obj;
-    } else {
-      obj.type = "checkbox";
-      decodeItems[key+":enabled"] = obj;
-    }
-  }
-
+  var decodeItems = decode.settings();
   res.send(JSON.stringify(decodeItems));
 });
 
